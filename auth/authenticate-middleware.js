8c233f014fd606b6ca9f2af656745d25a7edd7b1
ndurbin <nick.durbin@gmail.com>
2020-01-24 19:34:32 -0500

    Built the authenticate middleware.

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,24 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+const authenticate = (req, res, next) => {
+  const { authorization } = req.headers
+
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || 'A secret is a secret does.'
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: "Invalid Token!"})
+      } else {
+        req.token = decodedToken;
+
+        next()
+      }
+    })
+  } else {
+    res.status(400).json({ message: "You are not verified. Please login."})
+  }
+}
+
+module.exports = {
+  authenticate
+}
