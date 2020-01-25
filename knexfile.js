cff8da45329fdac6e83a4dde625c7b2c5a09e016
ndurbin <nick.durbin@gmail.com>
2020-01-24 19:07:48 -0500

    Added a test configuraiton.

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,30 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    connection: { 
+      filename: './database/auth.db3' 
+    },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { 
+      directory: './database/seeds' 
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
 };
