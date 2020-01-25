25f3f4ea9ec0ea761bd260d5f1c48fbeb2bee107
ndurbin <nick.durbin@gmail.com>
2020-01-24 19:04:33 -0500

    Configured the index.js to my liking with dotenv.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const server = require('./api/server.js');
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3300;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
-});
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 5000
+
+if (!module.parent) {
+  server.listen(PORT, () => {
+    console.log(`Server listening on PORT: ${PORT}`)
+  })
+}
