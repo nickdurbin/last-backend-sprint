98286c413f4fb3978f8287166fd38bd8ddda10c4
ndurbin <nick.durbin@gmail.com>
2020-01-24 19:59:05 -0500

    Built the router for register and login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,35 @@
-const router = require('express').Router();
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const { generateToken } = require('./generateToken')
+const Users = require('../users/user-model')
+const router = express.Router()
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+router.post("/register", async (req, res, next) => {
+  try {
+    const user = await Users.add(req.body)
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+    res.status(201).json({ message: "User has been successfully registered.", user})
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const user = await Users.findBy({ username }).first()
+    const passwordValid = await bcrypt.compare(password, user.password)
+
+    if (user && passwordValid) {
+      const token = generateToken(user);
+      res.status(200).json({ 
+        message: `Welcome, ${user.username}!`, token})
+    } else {
+      res.status(401).json({ message: "Please try to login again!"})
+    }
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
