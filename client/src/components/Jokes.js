d6ce7bef92c3ff3ae821c215fb16087a3ad5b463
nick.durbin <nick.durbin@gmail.com>
2020-01-27 14:07:09 -0500

    Created a jokes comp. But having issues.

---
+++ client/src/components/Jokes.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+function Jokes() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/api/jokes")
+      .then(res => {
+        console.log(res.data)
+        setJokes(res.data)
+      })
+      .catch(err => {
+        console.log(err, "No data")
+      })
+  }, [])
+
+  return (
+    <div className="jokesContainer">
+      <h1>How About A Joke?</h1>
+      {jokes.map(joke => {
+        console.log(joke)
+        return (
+          <div className="jokesList" key={joke.id}>
+            <h2>{joke.joke}</h2>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Jokes
