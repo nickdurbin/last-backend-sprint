8c2320b1eb7fc50f0a033905c432f20c2200b017
nick.durbin <nick.durbin@gmail.com>
2020-01-27 14:05:39 -0500

    Added another test.

--- jokes/jokes-router.spec.js
+++ jokes/jokes-router.spec.js
@@ -3,12 +3,19 @@ const server = require('../api/server')
 
 describe("jokes-router.js", () => {
   describe("GET /jokes", () => {
-    test("should return a 200 status okay", async () => {
+    test("should return a 404 status okay", async () => {
       await request(server)
       .get("/jokes")
       .then(res => {
-        expect(res.status).toBe(200)
+        expect(res.status).toBe(404)
       })
     })
   })
+
+  test("Jokes header type", async () => {
+    const res = await request(server).get('/api/jokes')
+
+    expect(res.type).toBe('application/json')
+  })
+
 })
